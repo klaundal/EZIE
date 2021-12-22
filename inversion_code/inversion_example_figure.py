be656edd6b5858f92d24a75e94c8cb65c1f07109
Karl Laundal <karl.laundal@ift.uib.no>
2021-12-22 18:36:22 +0100

    adapted script to new secsy module

--- inversion_code/inversion_example_figure.py
+++ inversion_code/inversion_example_figure.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from dipole import geo2mag
 from secsy import spherical 
-from simulation_utils import get_MHD_jeq, get_MHD_dB
+from simulation_utils import get_MHD_jeq, get_MHD_dB, get_MHD_dB_new
 import pandas as pd
 import os
 
@@ -21,7 +21,7 @@ info = {'filename':path + '/../data/proposal_stage_sam_data/EZIE_event_simulatio
         'mapshift':-210, # Sam has shifted the MHD output by this amount to get an orbit that crosses something interesting. The shift must be applied to my MHD readout functions
         'observation_height':80,
         'output_path':'final_figs/',
-        'wshift':25,
+        'wshift':-50,
         'tm':dt.datetime(2023, 7, 3, 2, 42, 22),
         'outputfn':'proposal_stage',
         'mhdfunc':get_MHD_dB,
@@ -30,21 +30,25 @@ info = {'filename':path + '/../data/proposal_stage_sam_data/EZIE_event_simulatio
 # OSSE CASE 1
 """
 info = {'filename':path + '/../data/OSSE_new/case_1/EZIE_event_simulation_CASE1_standard_EZIE_retrieved_los_mag_fields2.pd',
-        'mapshift':0, # Sam has shifted the MHD output by this amount to get an orbit that crosses something interesting. The shift must be applied to my MHD readout functions
+        'mhd_B_fn':path + '/../data/OSSE_new/case_1/gamera_dBs_80km_2016-08-09T08_49_45.txt',
+        'mapshift':180, # Sam has shifted the MHD output by this amount to get an orbit that crosses something interesting. The shift must be applied to my MHD readout functions
         'observation_height':80,
         'output_path':'final_figs/',
         'wshift':25,
         'tm':dt.datetime(2023, 7, 4, 5, 59, 51),
         'outputfn':'osse_case1',
+        'mhdfunc':get_MHD_dB_new,
         'clevels':np.linspace(-300, 300, 12)}
 """
+
+
 
 OBSHEIGHT = info['observation_height']
 
 d2r = np.pi / 180
 
-WRES = 40. # spatial resolution of SECS grid along satellite track
-LRES = 20. # spatial resolution perpendicular to satellite tarck
+LRES = 40. # spatial resolution of SECS grid along satellite track
+WRES = 20. # spatial resolution perpendicular to satellite tarck
 wshift = info['wshift'] # shift center of grid wshift km to the right of the satellite (rel to velocity)
 DT  = 4 # size of time window [min]
 RI  = (6371.2 + 110) * 1e3 # SECS height (m)
@@ -80,17 +84,18 @@ for t in [t0, tm, t1]:
                         np.sin(data.loc[t, 'sat_lat'] * d2r)]))
 
 # dimensions of analysis region/grid (in km)
-L = 400 + RI * np.arccos(np.sum(rs[0]*rs[-1])) * 1e-3 # km
-W = 1200
+W = 400 + RI * np.arccos(np.sum(rs[0]*rs[-1])) * 1e-3 # km
+L = 1200
 print(L, W)
 
 # set up the cubed sphere projection
-v  = (data.loc[tm, 've'], data.loc[tm, 'vn'])
-angle = np.arctan2(v[1], v[0]) / d2r + np.pi/2 
+v  = np.array((data.loc[tm, 've'], data.loc[tm, 'vn']))
+#angle = np.arctan2(v[1], v[0]) / d2r + np.pi/2 
 
+orientation = np.array([v[1], -v[0]]) # align coordinate system such that xi axis points right wrt to satellite velocity vector, and eta along velocity
 
 p = data.loc[tm, 'sat_lon'], data.loc[tm, 'sat_lat']
-projection = CSprojection(p, angle)
+projection = CSprojection(p, orientation)
 grid = CSgrid(projection, L, W, LRES, WRES, wshift = wshift)
 Le, Ln = grid.get_Le_Ln()
 LL = Le.T.dot(Le) # matrix for calculation of eastward gradient - eastward in magnetic since all coords above have been converted to dipole coords
@@ -152,21 +157,14 @@ axr_secs      = plt.subplot2grid((16, 2), (10, 1), rowspan = 5)
 ax_cbar = plt.subplot2grid((33, 2), (32, 0), colspan = 2)
 
 
-
+# plot the data tracks:
+ximin, ximax, etamin, etamax = 0, 0, 0, 0 # plot limits
 for i in range(4):
-    obs['lat'] += list(data.loc[t0:t1, 'lat_' + str(i + 1)].values)
-    obs['lon'] += list(data.loc[t0:t1, 'lon_' + str(i + 1)].values)
-    obs['Be' ] += list(data.loc[t0:t1, 'dbe_measured_'  + str(i + 1)].values)
-    obs['Bn' ] += list(data.loc[t0:t1, 'dbn_measured_'  + str(i + 1)].values)
-    obs['Bu' ] += list(data.loc[t0:t1, 'dbu_measured_'  + str(i + 1)].values)
 
     lon, lat = data.loc[t0:t1, 'lon_' + str(i + 1)].values, data.loc[t0:t1, 'lat_' + str(i + 1)].values
     xi, eta = projection.geo2cube(lon, lat)
     for ax in [axe_secs, axn_secs, axr_secs]:
-        ax.plot(eta, xi, color = 'C' + str(i), linewidth = 3)
-    if i == 0:
-        etamin, etamax = eta.min(), eta.max()
-        ximin, ximax = xi.min(), xi.max()
+        ax.plot(xi, eta, color = 'C' + str(i), linewidth = 3)
     else:
         if eta.min() < etamin:
             etamin = eta.min()
@@ -178,105 +176,98 @@ for i in range(4):
             ximax = xi.max()
 
 
-
-
-
-
+# set up G matrices for the magnetic field evaluated on a grid - for plotting maps
 Gde, Gdn, Gdu = get_SECS_B_G_matrices(grid.lat.flatten()+.1, grid.lon.flatten(), np.ones(grid.lon.size) * (6371.2 + OBSHEIGHT) * 1e3, 
                                       grid.lat.flatten(), grid.lon.flatten(), 
                                       current_type = 'divergence_free', RI = RI)
 
-mhdBu = get_MHD_dB(grid.lat.flatten(), grid.lon.flatten() + info['mapshift'], fn = info['mhd_B_fn'])
-mhdBe = get_MHD_dB(grid.lat.flatten(), grid.lon.flatten() + info['mapshift'], component = 'Bphi [nT]', fn = info['mhd_B_fn'])
-mhdBn = -get_MHD_dB(grid.lat.flatten(), grid.lon.flatten() + info['mapshift'], component = 'Btheta [nT]', fn = info['mhd_B_fn'])
-mhdB = np.sqrt(mhdBe**2 + mhdBn**2 + mhdBu**2).reshape(grid.lat.shape)
+# get maps of MHD magnetic fields:
+mhdBu =  info['mhdfunc'](grid.lat.flatten(), grid.lon.flatten() + info['mapshift'], fn = info['mhd_B_fn'])
+mhdBe =  info['mhdfunc'](grid.lat.flatten(), grid.lon.flatten() + info['mapshift'], component = 'Bphi [nT]', fn = info['mhd_B_fn'])
+mhdBn = -info['mhdfunc'](grid.lat.flatten(), grid.lon.flatten() + info['mapshift'], component = 'Btheta [nT]', fn = info['mhd_B_fn'])
 
+# plot magnetic field in upward direction (MHD and retrieved)
+cntrs = axr_secs.contourf(grid.xi, grid.eta, Gdu.dot(m).reshape(grid.shape), levels = info['clevels'], cmap = plt.cm.bwr, zorder = 0, extend = 'both')
+axr_true.contourf(grid.xi, grid.eta, mhdBu.reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
 
+# plot magnetic field in eastward direction (MHD and retrieved)
+axe_secs.contourf(grid.xi, grid.eta, Gde.dot(m).reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
+axe_true.contourf(grid.xi, grid.eta, mhdBe.reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
 
-B = np.sqrt(Gde.dot(m).reshape(grid.eta.shape)**2 + Gdn.dot(m).reshape(grid.eta.shape)**2 + Gdu.dot(m).reshape(grid.eta.shape)**2)
+# plot magnetic field in northward direction (MHD and retrieved)
+axn_secs.contourf(grid.xi, grid.eta, Gdn.dot(m).reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
+axn_true.contourf(grid.xi, grid.eta, mhdBn.reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
 
-
-
-cntrs = axr_secs.contourf(grid.eta, grid.xi, Gdu.dot(m).reshape(grid.shape), levels = info['clevels'], cmap = plt.cm.bwr, zorder = 0, extend = 'both')
-axr_true.contourf(grid.eta, grid.xi, mhdBu.reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
-
-axe_secs.contourf(grid.eta, grid.xi, Gde.dot(m).reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
-axe_true.contourf(grid.eta, grid.xi, mhdBe.reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
-
-axn_secs.contourf(grid.eta, grid.xi, Gdn.dot(m).reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
-axn_true.contourf(grid.eta, grid.xi, mhdBn.reshape(grid.shape), levels = cntrs.levels, cmap = plt.cm.bwr, zorder = 0, extend = 'both')
-
+# plot colorbar:
 ax_cbar.contourf(np.vstack((cntrs.levels, cntrs.levels)), np.vstack((np.zeros(cntrs.levels.size), np.ones(cntrs.levels.size))), np.vstack((cntrs.levels, cntrs.levels)), cmap = plt.cm.bwr, levels = cntrs.levels)
 ax_cbar.set_xlabel('nT')
 ax_cbar.set_yticks([])
 
+
+# calculate the equivalent current of retrieved magnetic field:
 jlat = grid.lat_mesh[::2, ::2].flatten()
 jlon = grid.lon_mesh[::2, ::2].flatten()    
 Gje, Gjn = get_SECS_J_G_matrices(jlat, jlon, grid.lat.flatten(), grid.lon.flatten(), current_type = 'divergence_free', RI = RI)
 je, jn = Gje.dot(m).flatten(), Gjn.dot(m).flatten()
 xi, eta, jxi, jeta = projection.vector_cube_projection(je.flatten(), jn.flatten(), jlon, jlat)
 
+# plot the equivalent current in the SECS panels:
 for ax in [axe_secs, axn_secs, axr_secs]:
-    ax.quiver(eta, xi, jeta, jxi, linewidth = 2, scale = 1e10, zorder = 40, color = 'black')#, scale = 1e10)
+    ax.quiver(xi, eta, jxi, jeta, linewidth = 2, scale = 1e10, zorder = 40, color = 'black')#, scale = 1e10)
 
-
-
+# get the MHD equivalent current field:
 mhd_je, mhd_jn = get_MHD_jeq(jlat, jlon + info['mapshift'])
 xi, eta, mhd_jxi, mhd_jeta = projection.vector_cube_projection(mhd_je, mhd_jn, jlon, jlat)
-#axmap.quiver(xi, eta, mhd_jxi, mhd_jeta, linewidth = 1, scale = 10, color = 'grey')#, scale = 1e10)
+
+# plot the MHD equivalent current in eaach panel
 for ax in [axe_true, axn_true, axr_true , axe_secs, axn_secs, axr_secs]:
-    ax.quiver(eta, xi, mhd_jeta, mhd_jxi, linewidth = 2, scale = 10, color = 'grey', zorder = 38)#, scale = 1e10)
+    ax.quiver(xi, eta, mhd_jxi, mhd_jeta, linewidth = 2, scale = 10, color = 'grey', zorder = 38)#, scale = 1e10)
 
 
-
+# plot coordinate grids, fix aspect ratio and axes in each panel
 for ax in [axe_secs, axe_true, axn_secs, axn_true, axr_secs, axr_true]:
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
     for l in np.r_[60:90:5]:
         xi, eta = projection.geo2cube(np.linspace(0, 360, 360), np.ones(360)*l)
-        ax.plot(eta, xi, color = 'lightgrey', linewidth = .5, zorder = 1)
+        ax.plot(xi, eta, color = 'lightgrey', linewidth = .5, zorder = 1)
 
-    for linewidth in np.r_[0:360:15]:
+    for l in np.r_[0:360:15]:
         xi, eta = projection.geo2cube(np.ones(360)*l, np.linspace(50, 90, 360))
-        ax.plot(eta, xi, color = 'lightgrey', linewidth = .5, zorder = 1)
+        ax.plot(xi, eta, color = 'lightgrey', linewidth = .5, zorder = 1)
 
-    #ax.set_xlim(ximin - 25/(RI * 1e-3), ximax + 25/(RI * 1e-3))
-    #ax.set_ylim(etamin, etamax)
     ax.axis('off')
 
     ax.set_adjustable('datalim') 
     ax.set_aspect('equal')
 
 
-
-#axe_true.set_title('MHD simulation output')
-#axe_secs.set_title('SECS inversion results')
-
+# Write labels:
 for ax, label in zip([axe_secs, axe_true, axn_secs, axn_true, axr_secs, axr_true],
                      ['Be SECS', 'Be MHD', 'Bn SECS', 'Bn MHD', 'Br SECS', 'Br MHD']):
     
-    ax.text(etamax- 25/(RI * 1e-3), ximin- 25/(RI * 1e-3), label, va = 'top', ha = 'left', bbox = dict(facecolor='white', alpha=1), zorder = 101, size = 14)
+    ax.text(ximin - 25/(RI * 1e-3), etamax - 25/(RI * 1e-3), label, va = 'top', ha = 'left', bbox = dict(facecolor='white', alpha=1), zorder = 101, size = 14)
 
 
 # plot grid in top left panel to show spatial dimensions:
 xigrid, etagrid = np.meshgrid(np.r_[grid.xi.min():grid.xi.max() + 200/RI*1e3:200/RI*1e3], 
                               np.r_[grid.eta.min():grid.eta.max() + 200/RI*1e3:200/RI*1e3])
 for i in range(xigrid.shape[0]):
-    axe_true.plot(etagrid[i], xigrid[i], 'k-', linewidth = .7)
+    axe_true.plot(xigrid[i], etagrid[i], 'k-', linewidth = .7)
 for j in range(xigrid.shape[1]):
-    axe_true.plot(etagrid[:, j], xigrid[:, j], 'k-', linewidth = .7)
+    axe_true.plot(xigrid[:, j], etagrid[:, j], 'k-', linewidth = .7)
 
-
+# set plot limits:
 for ax in [axe_secs, axe_true, axn_secs, axn_true, axr_secs, axr_true]:
-    ax.set_xlim(etamin - 25/(RI * 1e-3), etamax + 25/(RI * 1e-3))
-    ax.set_ylim(ximin + 55/(RI * 1e-3), ximax - 55/(RI * 1e-3))
+    ax.set_xlim(ximin - 25/(RI * 1e-3), ximax + 25/(RI * 1e-3))
+    ax.set_ylim(etamin + 55/(RI * 1e-3), etamax - 55/(RI * 1e-3))
     ax.set_adjustable('datalim') 
     ax.set_aspect('equal')
-    #ax.set_xlim(*xlim)
-    #ax.set_ylim(*ylim)
 
+# remove whitespace
 plt.subplots_adjust(bottom = .05, top = .99, left = .01, right = .99)
 
+# save plots:
 plt.savefig('./figures/' + info['outputfn'] + 'inversion_example.png', dpi = 250)
 plt.savefig('./figures/' + info['outputfn'] + 'inversion_example.pdf')
 
